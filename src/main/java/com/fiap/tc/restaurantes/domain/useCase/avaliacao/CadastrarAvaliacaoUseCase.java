b67c0868369335c6ee702543e23962de69af8924
caiotfernandes <caiotfernandes@gmail.com>
2024-09-09 17:54:38 -0300

    Refactor na validação avaliacao em CadastrarAvaliacaoUseCase

--- src/main/java/com/fiap/tc/restaurantes/domain/useCase/avaliacao/CadastrarAvaliacaoUseCase.java
+++ src/main/java/com/fiap/tc/restaurantes/domain/useCase/avaliacao/CadastrarAvaliacaoUseCase.java
@@ -1,6 +1,7 @@
 package com.fiap.tc.restaurantes.domain.useCase.avaliacao;
 
 import com.fiap.tc.restaurantes.domain.entity.Avaliacao;
+import com.fiap.tc.restaurantes.domain.entity.validations.AvaliacaoValidation;
 import com.fiap.tc.restaurantes.domain.gateway.avaliacao.CadastrarAvaliacaoInterface;
 import com.fiap.tc.restaurantes.domain.useCase.restaurante.BuscarRestaurantePorIdUseCase;
 import com.fiap.tc.restaurantes.domain.useCase.usuario.BuscarUsuarioPorIdUseCase;
@@ -24,10 +25,7 @@ public class CadastrarAvaliacaoUseCase {
         //Valida se Restaurate existe
         buscarRestaurantePorIdUseCase.execute(avaliacao.getRestaurante().getRestauranteId());
 
-        //Valida se nota é de 0 a 5
-        if (avaliacao.getNota() < 0 || avaliacao.getNota() > 5) {
-            throw new IllegalArgumentException("A Nota deve ser entre 0 e 5");
-        }
+        AvaliacaoValidation.validate(avaliacao);
 
         return cadastrarAvaliacaoInterface.cadastraAvaliacao(avaliacao);
     }
