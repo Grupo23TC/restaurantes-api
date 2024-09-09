761dcb845992c1a51a734e33ee8806f266230a15
caiotfernandes <caiotfernandes@gmail.com>
2024-09-09 11:21:13 -0300

    Removidas anotações lombok das classes core do projeto, adicionados contructors e getters/setters no lugar;

--- src/main/java/com/fiap/tc/restaurantes/domain/entity/Usuario.java
+++ src/main/java/com/fiap/tc/restaurantes/domain/entity/Usuario.java
@@ -1,16 +1,61 @@
 package com.fiap.tc.restaurantes.domain.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Usuario {
   private Long usuarioId;
   private String nome;
   private String email;
   private String senha;
   private String telefone;
+
+  public Usuario(Long usuarioId, String nome, String email, String senha, String telefone) {
+    this.usuarioId = usuarioId;
+    this.nome = nome;
+    this.email = email;
+    this.senha = senha;
+    this.telefone = telefone;
+  }
+
+  public Usuario() {
+  }
+
+  public Long getUsuarioId() {
+    return usuarioId;
+  }
+
+  public void setUsuarioId(Long usuarioId) {
+    this.usuarioId = usuarioId;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getSenha() {
+    return senha;
+  }
+
+  public void setSenha(String senha) {
+    this.senha = senha;
+  }
+
+  public String getTelefone() {
+    return telefone;
+  }
+
+  public void setTelefone(String telefone) {
+    this.telefone = telefone;
+  }
 }
